f18273172123f2fa9e0bd264a2223cdece239acd
Link1400 <ties_derby0d@icloud.com>
2024-04-11 22:05:29 -0400

    tessalation done, need to loop it and add second tessalation

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1920, 1080);
   }
 
   /** 
@@ -17,6 +17,7 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+    noLoop();
   }
 
   /**
@@ -25,12 +26,26 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    pushMatrix();
+    drawtri();
+    String postri = drawtess(frameRate, frameRate, frameRate, frameRate, frameRate, frameRate, frameRate, frameRate, frameRate);
+    System.out.println(postri);
+    popMatrix();
   }
   
   // define other methods down here.
+  public String drawtess(float trix1, float triy1, float trix2, float triy2, float trix3, float triy3, float col1, float col2, float col3){
+    pushMatrix();
+    fill(col1, col2, col3);
+    stroke(200);
+    triangle(trix1, triy1, trix2, triy2, trix3, triy3);
+    //rect(-trix1, -triy1, trix2, triy2);
+    return "DEBUG: Triangle tesselation segment completed";
+  }
+  public void drawtri(){
+    drawtess(100,0,200,100,100,100,100,200,0);
+    drawtess(100,0,200,100,0,100,100,200,0);
+    for(float triincrease 0){
+      
+  }
 }
